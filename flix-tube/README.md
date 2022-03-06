c0971b4c0d5d477c6d87d09ae6d9fda6bd7fe330
maria <maria.ic.mihai@gmail.com>
2022-03-06 13:31:39 +0100

    Added video and endpoint to stream it. Updated README as well.

--- flix-tube/README.md
+++ flix-tube/README.md
@@ -7,3 +7,9 @@ node index.js
 ````
 
 Test the service works correctly by checking in browser http://localhost:3000 or by using cURL with `curl http://localhost:3000`
+
+# Endpoints
+`/` - will return `Hello World!`
+
+`/video` - will return a video read from the local filesystem and streamed to the browser.
+Current implementation is verified with Chrome while other browsers might not work correctly at this time.
--- flix-tube/index.js
+++ flix-tube/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/video', (req, res) => {
+    const videoPath = './videos/SampleVideo_1280x720_1mb.mp4';
+
+    fs.stat(videoPath, (err, stats) => {
+
+        if(err) {
+            console.log('An error occurred');
+            res.sendStatus(500);
+
+            return;
+        }
+
+        res.writeHead(200, {
+            'Content-Length': stats.size,
+            'Content-Type': 'video/mp4'
+        });
+
+        fs.createReadStream(videoPath).pipe(res);
+    })
 });
 
 app.listen(port, () => {
